bbd537c00ecbecd81220dc818d059fa75ab1b1a9
prasanna lakshmi <prasannalakshmikathi@gmail.com>
2022-05-17 17:07:24 +0530

    [prasanna]: calculating greater than or not

--- linecomparison/linecomparison/Program.cs
+++ linecomparison/linecomparison/Program.cs
@@ -3,5 +3,12 @@ Console.WriteLine("Hello, World!");
 linecomparison.Line linecomparison = new linecomparison.Line();
 double length = linecomparison.getLength(2, 3, 2, 8);
 double length = linecomparison.getLength(2, 6, 2, 9);
-Console.WriteLine("length of the given line is" + length1,length2);
-Console.WriteLine(length1.Equals(length2));
+Console.WriteLine("length of the given line is" + L1, L2);
+Console.WriteLine(L1.Equals(L2));
+Console.WriteLine(L1.CompareTo(L2));
+if (L1.CompareTo(L2) > 0)
+    Console.WriteLine("The line one is greater than line two");
+else if (L1.CompareTo(L2) < 0)
+    Console.WriteLine("The line 2 is greater than line 1");
+else
+    Console.WriteLine("Both lines are same");
