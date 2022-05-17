542da37a79944dace1a1e2813cfa80076a3d4ba3
prasanna lakshmi <prasannalakshmikathi@gmail.com>
2022-05-17 16:53:51 +0530

    [prasanna]: checking equality of two lines

--- linecomparison/linecomparison/Program.cs
+++ linecomparison/linecomparison/Program.cs
@@ -2,4 +2,6 @@
 Console.WriteLine("Hello, World!");
 linecomparison.Line linecomparison = new linecomparison.Line();
 double length = linecomparison.getLength(2, 3, 2, 8);
-Console.WriteLine("length of the given line is" + length);
+double length = linecomparison.getLength(2, 6, 2, 9);
+Console.WriteLine("length of the given line is" + length1,length2);
+Console.WriteLine(length1.Equals(length2));
